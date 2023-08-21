3904c31149efa9d6e393d8ef03dd7079b56afac9
Cordoni_G <guidocordoni@yahoo.it>
2023-08-21 17:01:19 +0100

    Update Multifasta_to_Prokka.sh

--- Utilities/Multifasta_to_Prokka.sh
+++ Utilities/Multifasta_to_Prokka.sh
@@ -9,11 +9,29 @@ output_dir="your/path/prokka_results" #update here
 # Create the output directory if it doesn't exist
 mkdir -p $output_dir
 
-# Extract sample names from FASTA headers and iterate through them
-grep ">" $genome_multifasta | sed 's/>//' | while read sample_name; do
-    echo "Annotating sample: $sample_name"
-    mkdir -p $output_dir/$sample_name
-    prokka $genome_multifasta --outdir $output_dir/$sample_name --prefix $sample_name --force
-done
+# Iterate through each sequence in the multifasta file
+while read -r line; do
+    if [[ $line =~ ^\> ]]; then
+        if [[ -n $sample_dir ]]; then
+            # Run Prokka on the previous sample
+            sample_name=$(basename "$sample_fasta" .fasta)
+            prokka "$sample_fasta" --outdir "$sample_dir/$sample_name.prokka" --prefix "$sample_name" --debug
+        fi
+        sample_name=$(echo $line | sed 's/^>//')
+        sample_dir="$output_dir/$sample_name"
+        echo "Annotating sample: $sample_name"
+        mkdir -p $sample_dir
+        sample_fasta="$sample_dir/$sample_name.fasta"
+        echo $line > "$sample_fasta"
+    else
+        echo $line >> "$sample_fasta"
+    fi
+done < "$genome_multifasta"
+
+# Run Prokka on the last sample
+if [[ -n $sample_dir ]]; then
+    sample_name=$(basename "$sample_fasta" .fasta)
+    prokka "$sample_fasta" --outdir "$sample_dir/$sample_name.prokka" --prefix "$sample_name" --force --debug
+fi
 
 echo "Genome annotation completed."
