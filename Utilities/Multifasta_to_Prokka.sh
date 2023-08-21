29a8a733bee2d9781b906de2f24cde34102631e6
Cordoni_G <guidocordoni@yahoo.it>
2023-08-21 16:46:24 +0100

    Update Multifasta_to_Prokka.sh

--- Utilities/Multifasta_to_Prokka.sh
+++ Utilities/Multifasta_to_Prokka.sh
@@ -6,10 +6,14 @@ genome_multifasta="your/path/clean.full.fasta" #update here
 # Output directory for Prokka annotations
 output_dir="your/path/prokka_results" #update here
 
+# Create the output directory if it doesn't exist
+mkdir -p $output_dir
+
 # Extract sample names from FASTA headers and iterate through them
 grep ">" $genome_multifasta | sed 's/>//' | while read sample_name; do
     echo "Annotating sample: $sample_name"
-    prokka $genome_multifasta --outdir $output_dir/$sample_name --prefix $sample_name
+    mkdir -p $output_dir/$sample_name
+    prokka $genome_multifasta --outdir $output_dir/$sample_name --prefix $sample_name --force
 done
 
 echo "Genome annotation completed."
